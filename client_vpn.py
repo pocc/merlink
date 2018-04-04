a6abc4f06755e03267f856bba1ac53b0300b11ee
pocc <whim42+github@gmail.com>
2018-04-03 22:32:44 -0700

    UI now has a vertical layout and labels for textboxes

--- client_vpn.py
+++ client_vpn.py
@@ -1,30 +1,51 @@
 #!/usr/bin/python3
 # This program will connect desktop clients to Meraki firewalls
 import sys
-from PyQt5.QtWidgets import QApplication, QTextEdit, QWidget, QPushButton, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QApplication, QLineEdit, QWidget, QPushButton, QVBoxLayout, QLabel
 
 
 class MainWindow(QWidget):
 
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.instructions = QLabel("Please enter your Meraki Dashboard username & password:")
-        self.username = QTextEdit(self)
-        self.password = QTextEdit(self)
+        # Copying style from Dashboard Login (https://account.meraki.com/login/dashboard_login)
+        self.heading_style = "font-family: verdana, sans-serif; font-style: normal; " \
+                             "font-size: 28px;" \
+                             "font-weight: 300;" \
+                             "color:  #666;" \
+                             "background: #eee"
+        self.label_style = "font-family: verdana, sans-serif; font-style: normal; " \
+                           "font-size: 13px; " \
+                           "color: #000; " \
+                           "background: #eee;"
+        self.heading = QLabel("Client VPN Login")
+        self.heading.setStyleSheet(self.heading_style)
+        self.username_lbl = QLabel("Email")
+        self.username_lbl.setStyleSheet(self.label_style)
+        self.username = QLineEdit(self)
+        self.password_lbl = QLabel("Password")
+        self.password_lbl.setStyleSheet(self.label_style)
+        self.password = QLineEdit(self)
         self.connect_btn = QPushButton("Connect")
 
         self.init_ui()
 
     def init_ui(self):
-        layout = QVBoxLayout()
-        layout.addWidget(self.instructions)
-        layout.addWidget(self.username)
-        layout.addWidget(self.password)
-        layout.addWidget(self.connect_btn)
+        VLayout = QVBoxLayout()
+        VLayout.addWidget(self.heading)
+        VLayout.addWidget(self.username_lbl)
+        VLayout.addWidget(self.username)
+        VLayout.addWidget(self.password_lbl)
+        VLayout.addWidget(self.password)
+        VLayout.addWidget(self.connect_btn)
+        # Should prevent users from decreasing the size of the window past the minimum
+        VLayout.setSizeConstraint(QVBoxLayout.SetMinimumSize)
+        # Add a stretch so that all elements are at the top, regardless of user resizes
+        VLayout.addStretch()
 
         self.connect_btn.clicked.connect(self.make_connection)
 
-        self.setLayout(layout)
+        self.setLayout(VLayout)
         self.setWindowTitle('Meraki Client VPN')
 
         self.show()
