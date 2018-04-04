8c9edf83666520f8ea9617269afc4d74a61a7231
pocc <whim42+github@gmail.com>
2018-04-04 00:35:07 -0700

    Different background colors for main window and login layout

--- client_vpn.py
+++ client_vpn.py
@@ -14,14 +14,12 @@ class MainWindow(QWidget):
 
         # Copying style from Dashboard Login (https://account.meraki.com/login/dashboard_login)
         self.heading_style = "font-family: verdana, sans-serif; font-style: normal; " \
-                             "font-size: 28px;" \
-                             "font-weight: 300;" \
-                             "color:  #666;" \
-                             "background: #eee"
+                             "font-size: 28px; font-weight: 300; color:  #666;"
         self.label_style = "font-family: verdana, sans-serif; font-style: normal; " \
-                           "font-size: 13px; " \
-                           "color: #000; " \
-                           "background: #eee;"
+                           "font-size: 13px; color: #000;"
+        # self.login_btn_style = "border-radius: 2px; color: #fff; background: #7ac043"
+        self.link_style = "font-family: verdana, sans-serif; font-style: normal; font-size: 13px; color: #1795E6;"
+
         self.heading = QLabel("Dashboard Login")
         self.heading.setStyleSheet(self.heading_style)
         self.username_lbl = QLabel("Email")
@@ -30,11 +28,14 @@ class MainWindow(QWidget):
         self.password_lbl = QLabel("Password")
         self.password_lbl.setStyleSheet(self.label_style)
         self.password = QLineEdit(self)
-        self.connect_btn = QPushButton("Log in")
-        self.forgot_password = QLabel("I forgot my password")
-        self.forgot_password.setStyleSheet(self.label_style)
-        self.create_account = QLabel("Create an account")
-        self.create_account.setStyleSheet(self.label_style)
+        self.login_btn = QPushButton("Log in")
+        # self.login_btn.setStyleSheet(self.login_btn_style)
+        self.forgot_password = QLabel("<a href=\"https://account.meraki.com/login/reset_password\" "
+                                      "style=\"color:#1795E6;text-decoration:none\">I forgot my password</a>")
+        self.forgot_password.setStyleSheet(self.link_style)
+        self.create_account = QLabel("<a href=\"https://account.meraki.com/login/signup\" "
+                                     "style=\"color:#1795E6;text-decoration:none\">Create an account</a>")
+        self.create_account.setStyleSheet(self.link_style)
 
         self.init_ui()
 
@@ -43,26 +44,33 @@ class MainWindow(QWidget):
         layout_login_options.addWidget(self.forgot_password)
         layout_login_options.addStretch()
         layout_login_options.addWidget(self.create_account)
+        self.forgot_password.setOpenExternalLinks(True)
+        self.create_account.setOpenExternalLinks(True)
 
-        layout_login = QVBoxLayout()
+        login = QWidget(self)
+        login.setStyleSheet("background-color:white")
+        layout_login = QVBoxLayout(login)
         layout_login.addWidget(self.heading)
         layout_login.addWidget(self.username_lbl)
         layout_login.addWidget(self.username)
         layout_login.addWidget(self.password_lbl)
         layout_login.addWidget(self.password)
-        layout_login.addWidget(self.connect_btn)
+        layout_login.addWidget(self.login_btn)
         # Should prevent users from decreasing the size of the window past the minimum
         layout_login.setSizeConstraint(QVBoxLayout.SetMinimumSize)
         # Add a stretch so that all elements are at the top, regardless of user resizes
-        layout_login.addStretch()
         layout_login.addLayout(layout_login_options)
+        layout_login.addStretch()
+        layout_login.setProperty("background-color:white", self)
 
         self.meraki_img.setPixmap(QPixmap('meraki_connections.png'))
+        # Background for program will be #eee
+        self.setStyleSheet("background-color:#eee")
         layout_main = QHBoxLayout()
         layout_main.addWidget(self.meraki_img)
-        layout_main.addLayout(layout_login)
-
-        self.connect_btn.clicked.connect(self.make_connection)
+        #layout_main.addLayout(layout_login)
+        layout_main.addWidget(login)
+        self.login_btn.clicked.connect(self.make_connection)
 
         self.setLayout(layout_main)
         self.setWindowTitle('Meraki Client VPN')
