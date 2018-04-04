be02afdda5a332762ec4c92c2e922b3f8c62dd04
pocc <whim42+github@gmail.com>
2018-04-04 01:33:36 -0700

    Added link for about, which redirects to github project

--- client_vpn.py
+++ client_vpn.py
@@ -17,6 +17,7 @@ class MainWindow(QWidget):
                              "font-size: 28px; font-weight: 300; color:  #666;"
         self.label_style = "font-family: verdana, sans-serif; font-style: normal; " \
                            "font-size: 13px; color: #000;"
+        # Get back to login button style when you can change the color when it's clicked
         # self.login_btn_style = "border-radius: 2px; color: #fff; background: #7ac043"
         self.link_style = "font-family: verdana, sans-serif; font-style: normal; font-size: 13px; color: #1795E6;"
 
@@ -29,27 +30,34 @@ class MainWindow(QWidget):
         self.password_lbl.setStyleSheet(self.label_style)
         self.password = QLineEdit(self)
         self.login_btn = QPushButton("Log in")
+
         # self.login_btn.setStyleSheet(self.login_btn_style)
-        self.forgot_password = QLabel("<a href=\"https://account.meraki.com/login/reset_password\" "
-                                      "style=\"color:#1795E6;text-decoration:none\">I forgot my password</a>")
-        self.forgot_password.setStyleSheet(self.link_style)
-        self.create_account = QLabel("<a href=\"https://account.meraki.com/login/signup\" "
-                                     "style=\"color:#1795E6;text-decoration:none\">Create an account</a>")
-        self.create_account.setStyleSheet(self.link_style)
+        self.forgot_password_lbl = QLabel("<a href=\"https://account.meraki.com/login/reset_password\" "
+                                          "style=\"color:#1795E6;text-decoration:none\">I forgot my password</a>")
+        self.forgot_password_lbl.setStyleSheet(self.link_style)
+        self.forgot_password_lbl.setOpenExternalLinks(True)
+        self.create_account_lbl = QLabel("<a href=\"https://account.meraki.com/login/signup\" "
+                                         "style=\"color:#1795E6;text-decoration:none\">Create an account</a>")
+        self.create_account_lbl.setStyleSheet(self.link_style)
+        self.create_account_lbl.setOpenExternalLinks(True)
+        self.about_lbl = QLabel("<a href=\"https://github.com/pocc/meraki-client-vpn\" "
+                                "style=\"color:#1795E6;text-decoration:none\">About</a>")
+        self.about_lbl.setStyleSheet(self.link_style)
+        self.about_lbl.setOpenExternalLinks(True)
 
         self.init_ui()
 
     def init_ui(self):
         layout_login_options = QHBoxLayout()
-        layout_login_options.addWidget(self.forgot_password)
+        layout_login_options.addWidget(self.forgot_password_lbl)
         layout_login_options.addStretch()
-        layout_login_options.addWidget(self.create_account)
-        self.forgot_password.setOpenExternalLinks(True)
-        self.create_account.setOpenExternalLinks(True)
+        layout_login_options.addWidget(self.create_account_lbl)
 
-        login = QWidget(self)
-        login.setStyleSheet("background-color:white")
-        layout_login = QVBoxLayout(login)
+        # Create a widget to contain the login layout. This allows us to paint the background of the widget
+        login_widget = QWidget(self)
+        login_widget.setStyleSheet("background-color:white")
+        # Create labels and textboxes to form a login layout
+        layout_login = QVBoxLayout(login_widget)
         layout_login.addWidget(self.heading)
         layout_login.addWidget(self.username_lbl)
         layout_login.addWidget(self.username)
@@ -57,19 +65,17 @@ class MainWindow(QWidget):
         layout_login.addWidget(self.password)
         layout_login.addWidget(self.login_btn)
         # Should prevent users from decreasing the size of the window past the minimum
-        layout_login.setSizeConstraint(QVBoxLayout.SetMinimumSize)
         # Add a stretch so that all elements are at the top, regardless of user resizes
         layout_login.addLayout(layout_login_options)
         layout_login.addStretch()
-        layout_login.setProperty("background-color:white", self)
+        layout_login.addWidget(self.about_lbl)
 
         self.meraki_img.setPixmap(QPixmap('meraki_connections.png'))
-        # Background for program will be #eee
+        # Background for program will be #Meraki green = #78be20
         self.setStyleSheet("background-color:#eee")
         layout_main = QHBoxLayout()
         layout_main.addWidget(self.meraki_img)
-        #layout_main.addLayout(layout_login)
-        layout_main.addWidget(login)
+        layout_main.addWidget(login_widget)
         self.login_btn.clicked.connect(self.make_connection)
 
         self.setLayout(layout_main)
