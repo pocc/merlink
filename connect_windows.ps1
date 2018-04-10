aa0966c986b9fc103b0d0547cb3bfcfc07fff8b6
pocc <whim42+github@gmail.com>
2018-04-10 04:59:44 -0700

    added parameters and return if-else to powershell file

--- connect_windows.ps1
+++ connect_windows.ps1
@@ -1,5 +1,11 @@
 # Windows Powershell
-# Given an MX public IP, username and password, use powershell to create  a VPN connection
+# Given DDNS, public IP, username and password, use powershell to create  a VPN connection
+
+# Set the local variables to external parameters
+$ddns = $args[0]
+$mx_ip = $args[1]
+$username = $args[2]
+$password = $args[3]
 
 # Add the VPN connection
 Add-VpnConnection -name $vpn_name -ServerAddress $mx_ip
@@ -8,3 +14,14 @@ Set-VpnConnection -name $vpn_name -ServerAddress $mx_ip -AuthenticationMethod PA
 -RememberCredential $False -Force -WarningAction SilentlyContinue
 # rasdial connects a VPN (and is really old - it connects using a phonebook!)
 rasdial $vpn_name $username $password
+
+# Code for returning a value
+$connection_success = 1
+if ($connection_success) {
+    Write-host "Connection success!"
+    $LASTEXITCODE=1
+    }
+else {
+    Write-host "Connection failed!"
+    $LASTEXITCODE=0
+}
