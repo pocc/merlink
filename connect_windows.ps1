9b5900dbe682108db33517e594acc01ab186f015
Ross Jacobs <Ross.Jacobs@corp.meraki.com>
2018-04-10 20:34:10 -0700

    Program is verified as working with PS1 on Windows 64bit

--- connect_windows.ps1
+++ connect_windows.ps1
@@ -1,27 +1,34 @@
 # Windows Powershell
-# Given DDNS, public IP, username and password, use powershell to create  a VPN connection
+# Given vpn name, PSK, DDNS, public IP, username and password, use powershell to create  a VPN connection
 
 # Set the local variables to external parameters
-$ddns = $args[0]
-$mx_ip = $args[1]
-$username = $args[2]
-$password = $args[3]
+$vpn_name = $args[0]
+$psk = $args[1]
+$ddns = $args[2]
+$mx_ip = $args[3]
+$username = $args[4]
+$password = $args[5]
 
-# Add the VPN connection
-Add-VpnConnection -name $vpn_name -ServerAddress $mx_ip
+# While debugging, following command will print all variables
+# invoke-expression 'write-host "vpn_name:" $vpn_name "| psk:" $psk "| ddns:" $ddns "| mx_ip:" $mx_ip "| username:" $username "| password:" $password'
+
+# Add the VPN connection if it doesn't already exist, prevent add-vpnconnection from complaining about it
+$ErrorActionPreference = 'silentlycontinue'
+Add-VpnConnection -name $vpn_name -ServerAddress '0'
+$ErrorActionPreference = 'Continue'
+
 # Add required settings for Meraki VPN
 Set-VpnConnection -name $vpn_name -ServerAddress $mx_ip -AuthenticationMethod PAP -L2tpPsk $psk -TunnelType L2tp `
 -RememberCredential $False -Force -WarningAction SilentlyContinue
 # rasdial connects a VPN (and is really old - it connects using a phonebook!)
-rasdial $vpn_name $username $password
+$result = rasdial $vpn_name $username $password
 
-# Code for returning a value
-$connection_success = 1
-if ($connection_success) {
+write-host $result
+# If we are successfully or already connected, return success, otherwise failure
+if (($result -match 'successfully connected') -Or ($result -match 'already connected')) {
     Write-host "Connection success!"
     $LASTEXITCODE=1
-    }
-else {
+} else {
     Write-host "Connection failed!"
     $LASTEXITCODE=0
 }
