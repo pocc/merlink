847bba70a1c180d2e38281163e15ad2a2a21c137
Ross Jacobs <whim42+github@gmail.com>
2018-04-13 04:31:28 -0700

    split off login class into its own file, moved scripts to their own folder

---
+++ src/login_window.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python3
+
+# Qt5
+from PyQt5.QtWidgets import (QLineEdit, QWidget, QPushButton, QLabel,
+                             QVBoxLayout, QHBoxLayout, QDialog, QMessageBox)
+from PyQt5.QtGui import QPixmap
+import mechanicalsoup
+
+
+class LoginWindow(QDialog):
+    def __init__(self):
+
+        super(LoginWindow, self).__init__()
+
+        self.meraki_img = QLabel()
+
+        # Copying style from Dashboard Login (https://account.meraki.com/login/dashboard_login)
+        self.heading_style = "font-family: verdana, sans-serif; font-style: normal; " \
+                             "font-size: 28px; font-weight: 300; color:  #666;"
+        self.label_style = "font-family: verdana, sans-serif; font-style: normal; " \
+                           "font-size: 13px; color: #000;"
+        # Get back to login button style when you can change the color when it's clicked
+        # self.login_btn_style = "border-radius: 2px; color: #fff; background: #7ac043"
+        self.link_style = "font-family: verdana, sans-serif; font-style: normal; font-size: 13px; color: #1795E6;"
+
+        self.heading = QLabel("Dashboard Login")
+        self.heading.setStyleSheet(self.heading_style)
+        self.username_lbl = QLabel("Email")
+        self.username_lbl.setStyleSheet(self.label_style)
+        self.password_lbl = QLabel("Password")
+        self.password_lbl.setStyleSheet(self.label_style)
+        self.username_field = QLineEdit(self)
+        self.password_field = QLineEdit(self)
+        # Set up username and password so these vars have values
+        self.username = ''
+        self.password = ''
+
+        # Masks password as a series of dots instead of characters
+        self.password_field.setEchoMode(QLineEdit.Password)
+        self.login_btn = QPushButton("Log in")
+
+        # self.login_btn.setStyleSheet(self.login_btn_style)
+        self.forgot_password_lbl = QLabel("<a href=\"https://account.meraki.com/login/reset_password\" "
+                                          "style=\"color:#1795E6;text-decoration:none\">I forgot my password</a>")
+        self.forgot_password_lbl.setStyleSheet(self.link_style)
+        self.forgot_password_lbl.setOpenExternalLinks(True)
+        self.create_account_lbl = QLabel("<a href=\"https://account.meraki.com/login/signup\" "
+                                         "style=\"color:#1795E6;text-decoration:none\">Create an account</a>")
+        self.create_account_lbl.setStyleSheet(self.link_style)
+        self.create_account_lbl.setOpenExternalLinks(True)
+        self.about_lbl = QLabel("<a href=\"https://github.com/pocc/meraki-client-vpn\" "
+                                "style=\"color:#1795E6;text-decoration:none\">About</a>")
+        self.about_lbl.setStyleSheet(self.link_style)
+        self.about_lbl.setOpenExternalLinks(True)
+
+        self.init_ui()
+
+    def init_ui(self):
+        layout_login_options = QHBoxLayout()
+        layout_login_options.addWidget(self.forgot_password_lbl)
+        layout_login_options.addStretch()
+        layout_login_options.addWidget(self.create_account_lbl)
+
+        # Create a widget to contain the login layout. This allows us to paint the background of the widget
+        login_widget = QWidget(self)
+        login_widget.setStyleSheet("background-color:white")
+        # Create labels and textboxes to form a login layout
+        layout_login = QVBoxLayout(login_widget)
+        layout_login.addWidget(self.heading)
+        layout_login.addWidget(self.username_lbl)
+        layout_login.addWidget(self.username_field)
+        layout_login.addWidget(self.password_lbl)
+        layout_login.addWidget(self.password_field)
+        layout_login.addWidget(self.login_btn)
+        # Should prevent users from decreasing the size of the window past the minimum
+        # Add a stretch so that all elements are at the top, regardless of user resizes
+        layout_login.addLayout(layout_login_options)
+        layout_login.addStretch()
+        layout_login.addWidget(self.about_lbl)
+
+        self.meraki_img.setPixmap(QPixmap('./src/media/meraki_connections.png'))
+        # Background for program will be #Meraki green = #78be20
+        self.setStyleSheet("background-color:#eee")
+        layout_main = QHBoxLayout()
+        layout_main.addWidget(login_widget)
+        layout_main.addWidget(self.meraki_img)
+        self.setLayout(layout_main)
+        self.setWindowTitle('Meraki Client VPN')
+
+        # Test connection with a virtual browser
+        self.login_btn.clicked.connect(self.init_browser)
+
+    def init_browser(self):
+        self.username = self.username_field.text()
+        self.password = self.password_field.text()
+        # Instantiate browser
+        self.browser = mechanicalsoup.StatefulBrowser(
+            soup_config={'features': 'lxml'},  # Use the lxml HTML parser
+            raise_on_404=True,
+            user_agent='MyBot/0.1: mysite.example.com/bot_info',
+        )
+
+        # Navigate to login page
+        self.browser.open('https://account.meraki.com/login/dashboard_login')
+        form = self.browser.select_form()
+        self.browser["email"] = self.username
+        self.browser["password"] = self.password
+        form.choose_submit('commit')  # Click login button
+        resp = self.browser.submit_selected()  # resp should be '<Response [200]>'
+        self.result_url = self.browser.get_url()
+
+        # After setting everything up, let's see whether user authenticates correctly
+        self.attempt_login()
+
+    def attempt_login(self):
+        # URL contains /login/login if login failed
+        if '/login/login' not in self.result_url:
+            self.accept()
+        else:
+            # Error message popup that will take control and that the user will need to acknowledge
+            error_message = QMessageBox()
+            error_message.setIcon(QMessageBox.Critical)
+            error_message.setWindowTitle("Error!")
+            error_message.setText('ERROR: Invalid username or password.')
+            error_message.exec_()
+
+            # Sanitize password field so they can reenter credentials
+            ''' Design choice: Don't reset username as reentering can be annyoying if only password is wrong
+                self.username_field.setText('')'''
+            self.password_field.setText('')
+
+            # Don't return 'Rejected' as value from QDialog object as that will kill login window for auth fail
+            # self.reject()
+
+    # Return browser with any username, password, and cookies with it
+    def get_browser(self):
+        return self.browser
