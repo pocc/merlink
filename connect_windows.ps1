f44497528dcd80d589a23f88698e1eab8756c08c
Ross Jacobs <whim42+github@gmail.com>
2018-04-12 23:55:20 -0700

    Added split-tunnel code. Preferences dialog now has content

--- connect_windows.ps1
+++ connect_windows.ps1
@@ -8,9 +8,10 @@ $ddns = $args[2]
 $mx_ip = $args[3]
 $username = $args[4]
 $password = $args[5]
+$is_split_tunnel = $args[6]
 
 # While debugging, following command will print all variables
-# invoke-expression 'write-host "vpn_name:" $vpn_name "| psk:" $psk "| ddns:" $ddns "| mx_ip:" $mx_ip "| username:" $username "| password:" $password'
+# invoke-expression 'write-host "vpn_name:" $vpn_name "| psk:" $psk "| ddns:" $ddns "| mx_ip:" $mx_ip "| username:" $username "| password:" $password "| split_tunnel:" $is_split_tunnel'
 
 # Add the VPN connection if it doesn't already exist, prevent add-vpnconnection from complaining about it
 $ErrorActionPreference = 'silentlycontinue'
@@ -19,7 +20,7 @@ $ErrorActionPreference = 'Continue'
 
 # Add required settings for Meraki VPN
 Set-VpnConnection -name $vpn_name -ServerAddress $mx_ip -AuthenticationMethod PAP -L2tpPsk $psk -TunnelType L2tp `
--RememberCredential $False -Force -WarningAction SilentlyContinue
+-RememberCredential $False -SplitTunneling $is_split_tunnel -Force -WarningAction SilentlyContinue
 # rasdial connects a VPN (and is really old - it connects using a phonebook!)
 $result = rasdial $vpn_name $username $password
 
