df0f7e6afd50e979665b23a93bdc3469da35aa96
pocc <whim42+github@gmail.com>
2018-04-26 23:30:55 -0700

    Added split tunnel functionality, fixes #6

--- src/merlink.py
+++ src/merlink.py
@@ -636,13 +636,12 @@ class MainWindow(QMainWindow):
                 self.psk = self.psk.replace('$', '`$')
                 self.username = self.username.replace('$', '`$')
                 self.password = self.password.replace('$', '`$')
-                self.split_tunnel = '$False'
                 # Setting execution policy to unrestricted is necessary so that we can access VPN functions
                 # Sending DDNS and IP so if DDNS fails, windows can try IP as well
                 result = subprocess.call(
                     [powershell_path, '-ExecutionPolicy', 'Unrestricted',
                      self.cwd + '\scripts\connect_windows.ps1', vpn_name, self.psk, self.current_ddns,
-                     self.current_primary_ip, self.username, self.password, self.split_tunnel, str(DEBUG)]
+                     self.current_primary_ip, self.username, self.password, str(self.split_tunnel), str(DEBUG)]
                 )
                 print("MainWindow and the result is " + str(result) + str(type(result)))
                 if result == 0:
