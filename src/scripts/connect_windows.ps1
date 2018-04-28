b18e06b0cade4bb9009c921dbd19ccbf81c1a4f2
pocc <whim42+github@gmail.com>
2018-04-28 14:57:52 -0700

    Added documentation to ps script for options; Added remembercredential flag as parameter to ps script and use it in ps script

--- src/scripts/connect_windows.ps1
+++ src/scripts/connect_windows.ps1
@@ -1,9 +1,10 @@
 # Windows Powershell
-# Given vpn name, PSK, DDNS, public IP, username and password, use powershell to create  a VPN connection
+# Given VPN name, PSK, DDNS, public IP, username and password, use powershell to create  a VPN connection
 
 ##### ADDITIONAL SET-VPNCONNECTION PARAMETERS THAT ARE NOT BEING USED #####
+# Items that have been added to this script have a '+++'
 
-### Switch paramaters ###
+### Switch paramaters (doesn't take an argument) ###
 # -AllUserConnection : All users on the PC have access to this VPN connection | Flag with no value required
 # -AsJob (we can make this option available, but it shouldn't actually do anything
 # -PassThru (we can make this optino available, but it shouldn't actually do anything
@@ -11,19 +12,27 @@
 ### CimSession ###
 # -CimSession : Runs the cmdlet in a remote session or on a remote computer.
 
-### String (more) ###
+### String ###
+# +++ -Name : Name of the VPN connection
+# +++ -ServerAddress : Address of the Server
+# +++ -AuthenticationMethod : Authentication Type. For Meraki FWs, this is PAP
+# +++ -TunnelType : Type of the tunnel. For Meraki FWs, this is L2TP
+# +++ -L2tpPsk : Pre-Shared Key for L2TP
 # -DnsSuffix : contoso.com for server.contoso.com
 # -IdleDisconnectSeconds : sec after which connection will die
-# -RememberCredential : Save the credential so you don't have to reenter (true/false as string)
 # -ServerList : Array of servers that can be connected to
 # -UseWinlogonCredential : Use your widows login
+
+### $True/$False is passed in as string parameter ###
+# +++ -SplitTunneling : Go to the internet via your local connection instead of through the remote FW
+# -RememberCredential : Save the credential so you don't have to reenter
 
 ### 3rd party parameters (may be useful later) ###
 # -CustomConfiguration : Requires XML file but will pass parametrs to set-vpnconnection
 # -PlugInApplicationID : Specifies the identifier for a third party application.
 # -ThirdPartyVpn :  Indicates that the cmdlet runs for a third party profile.
 
-### Extra options that may be irrelevant ###
+### Extra options that will not likely be used ###
 # -MachineCertificateEKUFilter
 # -MachineCertificateIssuerFilter
 # -ThrottleLimit
@@ -34,14 +43,15 @@
 
 # Note that there is no limit on # of powershell ags, but around ~8192 characters on 64bit is a limit
 # Set the local variables to external parameters (all are coming in as string)
-$vpn_name = $args[0]    # String
+$vpn_name = $args[0]            # String
 $psk = $args[1]                 # String
 $ddns = $args[2]                # String
 $mx_ip = $args[3]               # String
 $username = $args[4]            # String
 $password = $args[5]            # String
 $split_tunnel_arg = $args[6]    # True/False (String)
-$DEBUG = $args[7]
+$DEBUG = $args[7]               # If DEBUG var is set in python
+$remember_cred_arg = $args[8]   # True/False (String)
 
 # While debugging, following command will print all variables
 if ($DEBUG -eq 'True') {
@@ -49,12 +59,18 @@ if ($DEBUG -eq 'True') {
 }
 
 # Manually assign boolean true for string true so we don't have to deal with it in parameter passing
-if ($split_tunnel_arg -eq 'True'){
-    $is_split_tunnel = $True
-}
-else {$is_split_tunnel = $False}
 
-# Disconnect from any other VPNs first
+# Set Split Tunnel var as PS bool
+if ($split_tunnel_arg -eq 'True') {
+        $is_split_tunnel = $True
+} else {$is_split_tunnel = $False}
+
+# Set RememberCredential var as PS bool
+if ($remember_cred_arg -eq 'True') {
+        $has_remember_credential = $True
+} else {$has_remember_credential = $False}
+
+# Disconnect from any active VPNs first
 foreach ($connection in Get-VpnConnection) {
     if (($connection | select -ExpandProperty ConnectionStatus) -eq 'Connected') {
         $vpn_name = $connection | select -ExpandProperty Name
@@ -69,7 +85,8 @@ $ErrorActionPreference = 'Continue'
 
 # Add required settings for Meraki VPN
 Set-VpnConnection -name $vpn_name -ServerAddress $mx_ip -AuthenticationMethod PAP -L2tpPsk $psk -TunnelType L2tp `
--RememberCredential $False -SplitTunneling $is_split_tunnel -Force -WarningAction SilentlyContinue
+-RememberCredential $has_remember_credential -SplitTunneling $is_split_tunnel -Force `
+-WarningAction SilentlyContinue
 # rasdial connects a VPN (and is really old - it connects using a phonebook!)
 $result = rasdial $vpn_name $username $password
 
