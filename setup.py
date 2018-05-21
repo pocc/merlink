5701a31f69b9fe5c915dcf77dec52436a183a2e3
Merakiuser <whim42+github@gmail.com>
2018-05-20 22:30:43 -0700

    Halfway save point for macOS. Added preference blob XML that we'll use to manually alter VPN XML file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='merlink',
+    version='0.5.6',
+    packages=['src', 'src.modules', 'src.scripts'],
+    url='pocc.github.io/merlink',
+    license='Apache 2',
+    author='Ross Jacobs',
+    author_email='merlinkproject@gmail.com',
+    description='cross-platform VPN client'
+)
