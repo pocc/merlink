fb5d72ca784270404d085d3595c7232c20a64a5a
Merakiuser <whim42+github@gmail.com>
2018-06-05 01:18:36 -0700

    macos can now connect fixes #33

--- src/scripts/connect_macos.sh
+++ src/scripts/connect_macos.sh
@@ -1,6 +1,6 @@
-#/bin/bash
+#!/bin/bash
 # This script takes 1 parameter - the name of a VPN connection that has already been created
-/usr/sbin/networksetup -connectpppoeservice $1
+/usr/sbin/networksetup -connectpppoeservice "$1"
 
 # Keeping this as backup in case Apple breaks networksetup
 # Applescript that will work provided you are using script editor
