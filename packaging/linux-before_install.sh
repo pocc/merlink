505aeef1da0a8cfb115f541e20ab041862faa972
pocc <whim42+github@gmail.com>
2018-06-27 04:39:44 -0700

    attempt #72 - readded content from deleted commit

--- packaging/linux-before_install.sh
+++ packaging/linux-before_install.sh
@@ -3,7 +3,12 @@
 cat /etc/apt/sources.list
 # sudo apt-get update MUST be run in before_install
 sudo apt-get update -qq
-# Required for trusty and xenial, but fails on bionic
-sudo apt install qt5-default
+# Required for trusty and xenial
+sudo apt-get install python3 python3-pip
+
+### FPM
 # Required for rpmbuild to build rpm with fpm
 sudo apt-get install -y rpm build-essential
+# Install ruby meta packages
+sudo apt-get install -y ruby ruby-dev rubygems-integration
+sudo gem install --no-ri --no-rdoc fpm
