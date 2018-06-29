66757b73d69dd9c30d3f4d7b5ad01ccad54f5e85
Merakiuser <whim42+github@gmail.com>
2018-06-29 03:47:55 -0700

    Added partial macos support to build script

---
+++ packaging/build-macos-linux-packages.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+# This script will build merlink targets using pyinstaller and fpm
+# When in doubt, use a force/yes flag to overwrite
+
+echo "Entering working directory: $(pwd)"
+# If we're not running in Travis CI, throw an error
+if [ -z $TRAVIS_TAG ]; then
+    TRAVIS_TAG='Leprechaun.Gold'
+fi
+NAME='merlink'
+
+### Create bundle with pyinstaller
+pyinstaller -y pyinstaller.linux.spec
+
+### Setup for FPM
+mkdir -pv bin/usr/bin
+mkdir -pv bin/opt
+# Create a symbolic link to the merlink binary in opt
+ln -fsv /opt/merlink/merlink bin/usr/bin
+# Pyinstaller generates a folder bundle in dist. Move this to opt.
+cp -fvr dist/merlink bin/opt
+
+### FPM configuration
+cd bin
+# deb + rpm + tar + osxpkg
+OPTIONS="--force --verbose \
+    --input-type dir \
+    --version ${TRAVIS_TAG} \
+    --name ${NAME} \
+    --maintainer projectmerlink@gmail.com \
+    --license 'opt/merlink/LICENSE.txt' \
+    --architecture amd64 \
+    --category Network \
+    --url pocc.merlink.io/merlink \
+	    --vendor MerLink"
+
+echo "Working directory before using fpm: $(pwd)"
+
+# If Linux make deb, rpm, tar; Elif Macos make dmg; Else Error
+if [ $(uname) = 'Linux' ]; then 
+	# Remove problematic file for 18.04 Ubuntu
+	rm -vf dist/merlink/libdrm.so.2
+
+	# strongswan-plugin-ssl required for Linux Mint 18.3 (other Xenial distros come with it by default)
+	# Ubuntu 18.04 doesn't have strongswan-plugin-ssl though, so not requiring
+	#   network-manager-l2tp may be helpful in troubleshooting but is not required
+	# Annoyingly, debian calls it "network-manager" while redhat calls it "NetworkManager",
+	#   so we need separate dependency lists
+	# folders (usr, opt) MUST be the last arguments
+	# There are no config files, thus --deb-no-default-config-files
+	fpm --output-type deb ${OPTIONS} -p ${NAME}-${TRAVIS_TAG}.deb --description 'Cross-platform VPN editor' \
+	    --deb-no-default-config-files \
+	    --deb-priority optional \
+	    --depends network-manager \
+	    --depends network-manager-l2tp \
+	    --deb-suggests network-manager-l2tp-gnome \
+	    --deb-suggests strongswan-plugin-openssl\
+	    opt usr
+	# redhat doesn't believe in suggests. There's a suggestion I'd like to make.
+	fpm --output-type rpm ${OPTIONS} -p ${NAME}-${TRAVIS_TAG}.rpm --description 'Cross-platform VPN editor' \
+	    --depends NetworkManager \
+	    --depends NetworkManager-l2tp\
+	    opt usr
+	# Do not require anything so tar can just work everywhere
+	tar czf ${NAME}-${TRAVIS_TAG}.tar.gz opt usr
+
+	# pacman (is failing on my system, but need confirmation)
+	# ERRORS:
+	#     Process failed: /bin/bash failed (exit code 127). Full command was:["/bin/bash", "-c", "LANG=C bsdtar -czf .MTREE --format=mtree --options='!all,use-set,type,uid,gid,mode,time,size,md5,sha256,link' var .PKGINFO"] {:level=>:error}
+	#fpm -f -s dir -t pacman -v ${TRAVIS_TAG} -n ${NAME} -p bin/${NAME}-${TRAVIS_TAG}.pacman bin/usr bin/opt
+	# .tar.gz.     Using fpm and not tar for consistency.
+
+elif [ $(uname) = 'Darwin' ]; then
+	fpm --output-type osxpkg ${OPTIONS} -p ${NAME}-${TRAVIS_TAG}.pkg --description 'Cross-platform VPN editor' opt usr	
+else
+	echo "ERROR: Unsupported OS $(uname)"
+fi
+
