67168e0bc2320289903a0995c28563fda506a21a
Ross Jacobs <whim42+github@gmail.com>
2018-07-03 22:48:15 -0700

    adding dmgbuild to pip install

--- pkg/macos_before_install.sh
+++ pkg/macos_before_install.sh
@@ -4,6 +4,6 @@ brew update
 brew install python3
 # Install pip and venv and then activate
 python3 -m easy_install pip
-python3 -m pip install virtualenv
+python3 -m pip install virtualenv dmgbuild
 python3 -m virtualenv venv
 source venv/bin/activate
