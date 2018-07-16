a4f409699e3c471b0e9b9c9da902985f0f8ed55a
pocc <whim42+github@gmail.com>
2018-07-16 07:05:25 -0700

    WIP: Moving webscraping to a data_scraper from both login_window and merlink_gui

---
+++ src/modules/data_scraper.py
@@ -0,0 +1,296 @@
+#!/usr/bin/python3
+
+# This class will provide all the functionality for getting necessary data from websites
+
+
+class DataScraper:
+    def __init__(self):
+        super(DataScraper, self).__init__()
+        pass
+
+    # TODO login_gui #######################################################################
+    # This function will get the organizations and then save them as a dict of names and links
+    def scrape_orgs(self):
+        """ ASSERTS
+        Don't set data for network-only admins as they don't have org-access.
+        Network-only admin data is added in get_networks().
+        """
+
+        def init_browser(self):
+            self.username = self.username_field.text()
+            self.password = self.password_field.text()
+            # Instantiate browser
+            self.browser = mechanicalsoup.StatefulBrowser(
+                soup_config={'features': 'lxml'},  # Use the lxml HTML parser
+                raise_on_404=True,
+                user_agent='MyBot/0.1: mysite.example.com/bot_info',
+            )
+
+            # Navigate to login page
+            self.browser.open('https://account.meraki.com/login/dashboard_login')
+            form = self.browser.select_form()
+            self.browser["email"] = self.username
+            self.browser["password"] = self.password
+            form.choose_submit('commit')  # Click login button
+            resp = self.browser.submit_selected()  # resp should be '<Response [200]>'
+            self.result_url = self.browser.get_url()
+
+            # After setting everything up, let's see whether user authenticates correctly
+            self.attempt_login()
+
+        def attempt_login(self):
+            if '/login/login' in self.result_url:  # URL contains /login/login if login failed
+                # Error message popup that will take control and that the user will need to acknowledge
+                error_dialog('ERROR: Invalid username or password.')
+
+                # Sanitize password field so they can reenter credentials
+                ''' Design choice: Don't reset username as reentering can be annyoying if only password is wrong
+                    self.username_field.setText('')'''
+                self.password_field.setText('')
+
+                # Don't return 'Rejected' as value from QDialog object as that will kill login window for auth fail
+                # self.reject()
+            elif 'sms_auth' in self.result_url:  # Two-Factor redirect: https://account.meraki.com/login/sms_auth?go=%2F
+                # QDialog that gets 6 digit two-factor code
+                self.twofactor_dialog = QDialog()
+                self.twofactor_dialog.setWindowTitle("Two-Factor Authentication")
+                dialog_layout = QVBoxLayout()
+                twofactor_code_layout = QHBoxLayout()
+                self.twofactor_code_label = QLabel("Enter verification code")
+                self.get_twofactor_code = QLineEdit()
+
+                # Remember choice doesn't quite work yet
+                # self.get_remember_choice = QCheckBox("Remember verification for this computer for 30 days.")
+
+                self.twofactor_dialog_yesno = QHBoxLayout()
+                yesbutton = QPushButton("Verify")
+                yesbutton.setToolTip("Attempt connection with this two-factor code")
+                nobutton = QPushButton("Cancel")
+                yesbutton.setToolTip("Quit")
+                self.twofactor_dialog_yesno.addWidget(yesbutton)
+                self.twofactor_dialog_yesno.addWidget(nobutton)
+
+                # Layout code
+                twofactor_code_layout.addWidget(self.twofactor_code_label)
+                twofactor_code_layout.addWidget(self.get_twofactor_code)
+                dialog_layout.addLayout(twofactor_code_layout)
+                # dialog_layout.addWidget(self.get_remember_choice)
+                dialog_layout.addLayout(self.twofactor_dialog_yesno)
+                self.twofactor_dialog.setLayout(dialog_layout)
+
+                # self.twofactor_dialog.setModal(True)  # Forces you to interact with the dialog (desired behavior)
+                yesbutton.clicked.connect(self.submit_twofactor_info)  # wait on user to submit information to go here
+                nobutton.clicked.connect(self.quit_dialog)  # wait on user to submit information to go here
+                self.twofactor_dialog.exec_()
+
+            else:
+                self.accept()
+
+        def submit_twofactor_info(self):
+            form = self.browser.select_form()
+            self.browser["code"] = self.get_twofactor_code.text()
+            # if self.get_remember_choice.isChecked():
+            # self.browser["remember"] = "1"  # Set remember to checked by default
+            form.choose_submit('commit')  # Click 'Verify' button
+            self.browser.submit_selected()
+
+            current_page = self.browser.get_current_page().text
+            twofactor_success = current_page.find("Invalid verification code")  # Will return -1 if it is not found
+            if twofactor_success == -1:  # if success
+                self.quit_dialog()  # Kill the dialog because we don't need it any longer
+                self.accept()
+            else:
+                error_dialog('ERROR: Invalid verification code')
+                self.attempt_login()  # Try again. There is a recursion risk here.
+
+            # Return browser with any username, password, and cookies with it
+
+        def get_browser(self):
+            return self.browser
+
+        def quit_dialog(self):
+            self.twofactor_dialog.close()
+
+
+        # TODO merlink_gui #############################################################
+
+        if DEBUG:
+            print("In fn [scrape_orgs()]")
+
+        # NOTE: Until you choose an organization, Dashboard will not let you visit pages you should have access to
+        page = self.browser.get_current_page()
+        pagetext = page.text  # Use pagetext variable so we can have a string we can use slices with
+        is_one_org_admin_index = pagetext.find("org_str")  # Found in HTML for administrators with access to only 1 org
+        if is_one_org_admin_index != -1:  # org_str is ONLY found for one org admins
+            self.org_qty = 1
+        else:
+            # Get a list of all org links
+            org_names = page.findAll('a', href=re.compile('/login/org_choose\?eid=.{6}'))
+            # Get the number of orgs
+            self.org_qty = len(org_names)
+
+        if self.org_qty == 1:  # org admin with one org
+            org_name_start = pagetext.find("Mkiconf.org_name") + 20  # This should be present in EVERY dashboard page
+            org_name_end = org_name_start + pagetext[org_name_start:].find('\"')
+            one_org_name = pagetext[org_name_start: org_name_end]
+            self.org_links[one_org_name] = self.browser.get_url()
+            self.org_list.append(one_org_name)
+        elif self.org_qty > 1:  # 2+ Orgs admin
+            for i in range(self.org_qty):
+                org_str = str(org_names[i])
+                # 39:-4 = Name, 9:37 = Link
+                self.org_links[org_str[39:-4]] = 'https://account.meraki.com' + org_str[9:37]
+                self.org_list.append(org_str[39:-4])
+                print(org_str[39:-4] + self.org_links[org_str[39:-4]])
+
+        # Create as many network lists in the network list as there are orgs
+        self.network_list = [[]] * self.org_qty
+        self.base_url_list = [[]] * self.org_qty
+
+    def get_networks(self):
+        """ ASSERTS
+        * get_networks should only be called on initial startup or if a different organization has been chosen
+        * browser should be initialized
+        * browser should have clicked on an org in the org selection page so we can browse relative paths of an org
+        """
+        if DEBUG:
+            print("In get_networks")
+
+        self.status.showMessage("Status: Fetching networks in " + self.current_org + "...")
+
+        # If we're dealing with org admins
+        if not self.network_admin_only:
+            # This method will get the networks by using the administered_orgs json blob
+            current_url = self.org_links[self.current_org]
+            self.browser.open(current_url)
+        current_url = self.browser.get_url()
+        # base url is up to '/manage/'
+        upper_url_index = current_url.find('/manage')  # boundary between network string and webpage string
+        lower_url_index = current_url.find('.com/')  # boundary between domain string and network string
+        url_domain_part = current_url[:lower_url_index + 4]  # Add 4 for '.com'
+        url_network_part = current_url[lower_url_index + 4:upper_url_index + 7]  # Add 7 for '/manage'
+        # For this URL, it doesn't matter which network in an org we get it from because it will be the same
+        administered_orgs = url_domain_part + url_network_part + '/organization/administered_orgs'
+        self.browser.open(administered_orgs)
+        if DEBUG:
+            print(administered_orgs)
+
+        cj = self.browser.get_cookiejar()
+        if DEBUG:
+            print(cj)
+        response = requests.get(administered_orgs, cookies=cj)
+        administered_orgs_text = response.text
+
+        orgs_dict = json.loads(administered_orgs_text)
+        if DEBUG:
+            print("orgs_dict " + str(orgs_dict))
+            print("current org " + str(self.current_org))
+
+        # For network_only_admins, we first get org info from the administered_orgs page
+        if self.network_admin_only:
+            self.org_qty = len(orgs_dict.keys())
+            for i in range(self.org_qty):
+                org_id = list(orgs_dict)[i]
+                self.org_list.append(orgs_dict[org_id]['name'])
+            # Duplicating this line here as we're ok with network admins running this code multiple times as it's
+            # dependent on administerd_orgs json. For org admins, we keep it in scrape_orgs() so it runs once
+            self.network_list = [[]] * self.org_qty
+            self.base_url_list = [[]] * self.org_qty
+        if DEBUG:
+            print("org_qty " + str(self.org_qty))
+        for i in range(self.org_qty):  # For every organization
+            this_org = list(orgs_dict)[i]  # get this org's id
+            num_networks = orgs_dict[this_org]['num_networks']  # int of num networks
+            # Inner dict that contains base64 network name and all network info
+            node_groups = orgs_dict[this_org]['node_groups']
+            # List of network ids in base64. These network ids are keys for network data dict that is the value
+            network_base64_ids = list(node_groups.keys())
+            # Start out with no network names or network base urls for each organization
+            network_names = []
+            network_base_urls = []
+            # For orgs that are not the current org, we will get the number of networks, but get node_groups of {}
+            if node_groups == {}:
+                num_networks = 0
+            for j in range(num_networks):
+                node_group_data = node_groups[network_base64_ids[j]]
+                if node_group_data['network_type'] == 'wired' and not node_group_data['is_config_template']:
+                    network_names.append(node_group_data['n'])
+                    # Reconstructing the base url. 't' is for network name as it appears in URL
+                    network_base_urls.append(url_domain_part + '/' + node_group_data['t'] +
+                                             '/n/' + node_group_data['eid'] + '/manage')
+
+            # If that network list is empty, then fill it with the network names
+            if DEBUG:
+                print("self.current_org_index " + str(self.current_org_index))
+                print(self.network_list)
+            if self.network_list[self.current_org_index] == []:
+                if DEBUG:
+                    print("Adding networks to list")
+                self.network_list[self.current_org_index] = network_names
+                self.base_url_list[self.current_org_index] = network_base_urls
+
+            if DEBUG:
+                print(self.network_list[i])
+
+        self.refresh_network_dropdown()
+
+
+    def scrape_vars(self):
+        """
+        This method will scrape two things
+            + Primary WAN IP address
+            + Pre-shared key
+        This method will check these things
+            + Is client VPN enabled in dashboard?
+            - Is this a security appliance that is online?
+        """
+        self.get_client_vpn_text()
+
+        self.psk = self.client_vpn_soup.find("input", {"id": "wired_config_client_vpn_secret", "value": True})['value']
+        # Found in html as    ,"client_vpn_enabled":true
+        client_vpn_value_index = self.client_vpn_text.find(",\"client_vpn_enabled\"")
+
+        print(self.client_vpn_text[client_vpn_value_index:client_vpn_value_index+27])
+
+        self.scrape_ddns_and_ip()
+        # tshoot_vpn_fail_gui() MUST come after scrape_ddns_and_ip() because it needs DDNS/IP address
+        self.tshoot_vpn_fail_gui()
+
+    def get_client_vpn_text(self):
+        if DEBUG:
+            print("network dropdown index: " + str(self.network_dropdown.currentIndex()-1))
+        current_network_index = self.network_dropdown.currentIndex()-1  # Because dropdown has first option 'select'
+        self.current_network = str(self.network_list[self.current_org_index][current_network_index])
+        self.client_vpn_url = self.base_url_list[self.current_org_index][current_network_index] \
+            + '/configure/client_vpn_settings'
+        self.status.showMessage("Status: Fetching network data for " + self.current_network + "...")
+        print("Client VPN url " + self.client_vpn_url)
+
+        self.client_vpn_text = self.browser.get(self.client_vpn_url).text
+        self.client_vpn_soup = bs4.BeautifulSoup(self.client_vpn_text, 'lxml')
+
+    def scrape_ddns_and_ip(self):
+        """ ASSERTS
+        * This method gets ddns and ip values for the current network
+        * This method should ONLY be called if the user has hit the connect button
+
+        Features
+        - Get DDNS name (if enabled)
+        - Get primary interface's IP address
+        - Verify that virtual_ip == {"public_ip":
+        :return:
+        """
+        fw_status_url = self.base_url_list[self.current_org_index][self.network_dropdown.currentIndex()-1] \
+                        + '/nodes/new_wired_status'
+        self.fw_status_text = self.browser.get(fw_status_url).text
+
+        # ddns value can be found by searching for '"dynamic_dns_name"'
+        ddns_value_start = self.fw_status_text.find("dynamic_dns_name")+19
+        ddns_value_end = self.fw_status_text[ddns_value_start:].find('\"') + ddns_value_start
+        self.current_ddns=self.fw_status_text[ddns_value_start: ddns_value_end]
+
+        # Primary will always come first, so using find should find it's IP address, even if there's a warm spare
+        # Using unique '{"public_ip":' to find primary IP address
+        ip_start = self.fw_status_text.find("{\"public_ip\":")+14
+        ip_end = self.fw_status_text[ip_start:].find('\"') + ip_start
+        self.current_primary_ip=self.fw_status_text[ip_start: ip_end]
--- src/modules/merlink_gui.py
+++ src/modules/merlink_gui.py
@@ -96,133 +96,6 @@ class MainWindow(QMainWindow):
         self.main_init_ui()
         self.menu_bars()
 
-    # This function will get the organizations and then save them as a dict of names and links
-    def scrape_orgs(self):
-        """ ASSERTS
-        Don't set data for network-only admins as they don't have org-access.
-        Network-only admin data is added in get_networks().
-        """
-
-        if DEBUG:
-            print("In fn [scrape_orgs()]")
-
-        # NOTE: Until you choose an organization, Dashboard will not let you visit pages you should have access to
-        page = self.browser.get_current_page()
-        pagetext = page.text  # Use pagetext variable so we can have a string we can use slices with
-        is_one_org_admin_index = pagetext.find("org_str")  # Found in HTML for administrators with access to only 1 org
-        if is_one_org_admin_index != -1:  # org_str is ONLY found for one org admins
-            self.org_qty = 1
-        else:
-            # Get a list of all org links
-            org_names = page.findAll('a', href=re.compile('/login/org_choose\?eid=.{6}'))
-            # Get the number of orgs
-            self.org_qty = len(org_names)
-
-        if self.org_qty == 1:  # org admin with one org
-            org_name_start = pagetext.find("Mkiconf.org_name") + 20  # This should be present in EVERY dashboard page
-            org_name_end = org_name_start + pagetext[org_name_start:].find('\"')
-            one_org_name = pagetext[org_name_start: org_name_end]
-            self.org_links[one_org_name] = self.browser.get_url()
-            self.org_list.append(one_org_name)
-        elif self.org_qty > 1:  # 2+ Orgs admin
-            for i in range(self.org_qty):
-                org_str = str(org_names[i])
-                # 39:-4 = Name, 9:37 = Link
-                self.org_links[org_str[39:-4]] = 'https://account.meraki.com' + org_str[9:37]
-                self.org_list.append(org_str[39:-4])
-                print(org_str[39:-4] + self.org_links[org_str[39:-4]])
-
-        # Create as many network lists in the network list as there are orgs
-        self.network_list = [[]] * self.org_qty
-        self.base_url_list = [[]] * self.org_qty
-
-    def get_networks(self):
-        """ ASSERTS
-        * get_networks should only be called on initial startup or if a different organization has been chosen
-        * browser should be initialized
-        * browser should have clicked on an org in the org selection page so we can browse relative paths of an org
-        """
-        if DEBUG:
-            print("In get_networks")
-
-        self.status.showMessage("Status: Fetching networks in " + self.current_org + "...")
-
-        # If we're dealing with org admins
-        if not self.network_admin_only:
-            # This method will get the networks by using the administered_orgs json blob
-            current_url = self.org_links[self.current_org]
-            self.browser.open(current_url)
-        current_url = self.browser.get_url()
-        # base url is up to '/manage/'
-        upper_url_index = current_url.find('/manage')  # boundary between network string and webpage string
-        lower_url_index = current_url.find('.com/')  # boundary between domain string and network string
-        url_domain_part = current_url[:lower_url_index + 4]  # Add 4 for '.com'
-        url_network_part = current_url[lower_url_index + 4:upper_url_index + 7]  # Add 7 for '/manage'
-        # For this URL, it doesn't matter which network in an org we get it from because it will be the same
-        administered_orgs = url_domain_part + url_network_part + '/organization/administered_orgs'
-        self.browser.open(administered_orgs)
-        if DEBUG:
-            print(administered_orgs)
-
-        cj = self.browser.get_cookiejar()
-        if DEBUG:
-            print(cj)
-        response = requests.get(administered_orgs, cookies=cj)
-        administered_orgs_text = response.text
-
-        orgs_dict = json.loads(administered_orgs_text)
-        if DEBUG:
-            print("orgs_dict " + str(orgs_dict))
-            print("current org " + str(self.current_org))
-
-        # For network_only_admins, we first get org info from the administered_orgs page
-        if self.network_admin_only:
-            self.org_qty = len(orgs_dict.keys())
-            for i in range(self.org_qty):
-                org_id = list(orgs_dict)[i]
-                self.org_list.append(orgs_dict[org_id]['name'])
-            # Duplicating this line here as we're ok with network admins running this code multiple times as it's
-            # dependent on administerd_orgs json. For org admins, we keep it in scrape_orgs() so it runs once
-            self.network_list = [[]] * self.org_qty
-            self.base_url_list = [[]] * self.org_qty
-        if DEBUG:
-            print("org_qty " + str(self.org_qty))
-        for i in range(self.org_qty):  # For every organization
-            this_org = list(orgs_dict)[i]  # get this org's id
-            num_networks = orgs_dict[this_org]['num_networks']  # int of num networks
-            # Inner dict that contains base64 network name and all network info
-            node_groups = orgs_dict[this_org]['node_groups']
-            # List of network ids in base64. These network ids are keys for network data dict that is the value
-            network_base64_ids = list(node_groups.keys())
-            # Start out with no network names or network base urls for each organization
-            network_names = []
-            network_base_urls = []
-            # For orgs that are not the current org, we will get the number of networks, but get node_groups of {}
-            if node_groups == {}:
-                num_networks = 0
-            for j in range(num_networks):
-                node_group_data = node_groups[network_base64_ids[j]]
-                if node_group_data['network_type'] == 'wired' and not node_group_data['is_config_template']:
-                    network_names.append(node_group_data['n'])
-                    # Reconstructing the base url. 't' is for network name as it appears in URL
-                    network_base_urls.append(url_domain_part + '/' + node_group_data['t'] +
-                                             '/n/' + node_group_data['eid'] + '/manage')
-
-            # If that network list is empty, then fill it with the network names
-            if DEBUG:
-                print("self.current_org_index " + str(self.current_org_index))
-                print(self.network_list)
-            if self.network_list[self.current_org_index] == []:
-                if DEBUG:
-                    print("Adding networks to list")
-                self.network_list[self.current_org_index] = network_names
-                self.base_url_list[self.current_org_index] = network_base_urls
-
-            if DEBUG:
-                print(self.network_list[i])
-
-        self.refresh_network_dropdown()
-
     def change_organization(self):
         if self.org_dropdown.currentIndex() != 0:  # We only care if they've actually selected an organization
             self.network_dropdown.setEnabled(True)
@@ -252,27 +125,6 @@ class MainWindow(QMainWindow):
         self.network_dropdown.addItems(["-- Select a Network --"])
         self.network_dropdown.addItems(self.network_list[self.current_org_index])
 
-    def scrape_vars(self):
-        """
-        This method will scrape two things
-            + Primary WAN IP address
-            + Pre-shared key
-        This method will check these things
-            + Is client VPN enabled in dashboard?
-            - Is this a security appliance that is online?
-        """
-        self.get_client_vpn_text()
-
-        self.psk = self.client_vpn_soup.find("input", {"id": "wired_config_client_vpn_secret", "value": True})['value']
-        # Found in html as    ,"client_vpn_enabled":true
-        client_vpn_value_index = self.client_vpn_text.find(",\"client_vpn_enabled\"")
-
-        print(self.client_vpn_text[client_vpn_value_index:client_vpn_value_index+27])
-
-        self.scrape_ddns_and_ip()
-        # tshoot_vpn_fail_gui() MUST come after scrape_ddns_and_ip() because it needs DDNS/IP address
-        self.tshoot_vpn_fail_gui()
-
     def tshoot_vpn_fail_gui(self):
         self.status.showMessage("Status: Verifying configuration for " + self.current_network + "...")
         result = TroubleshootVpnFailure(self.fw_status_text, self.client_vpn_text,
@@ -301,45 +153,6 @@ class MainWindow(QMainWindow):
             # we might want to turn this into an error method with params
 
         self.status.showMessage("Status: Ready to connect to " + self.current_network + ".")
-
-    def get_client_vpn_text(self):
-        if DEBUG:
-            print("network dropdown index: " + str(self.network_dropdown.currentIndex()-1))
-        current_network_index = self.network_dropdown.currentIndex()-1  # Because dropdown has first option 'select'
-        self.current_network = str(self.network_list[self.current_org_index][current_network_index])
-        self.client_vpn_url = self.base_url_list[self.current_org_index][current_network_index] \
-            + '/configure/client_vpn_settings'
-        self.status.showMessage("Status: Fetching network data for " + self.current_network + "...")
-        print("Client VPN url " + self.client_vpn_url)
-
-        self.client_vpn_text = self.browser.get(self.client_vpn_url).text
-        self.client_vpn_soup = bs4.BeautifulSoup(self.client_vpn_text, 'lxml')
-
-    def scrape_ddns_and_ip(self):
-        """ ASSERTS
-        * This method gets ddns and ip values for the current network
-        * This method should ONLY be called if the user has hit the connect button
-
-        Features
-        - Get DDNS name (if enabled)
-        - Get primary interface's IP address
-        - Verify that virtual_ip == {"public_ip":
-        :return:
-        """
-        fw_status_url = self.base_url_list[self.current_org_index][self.network_dropdown.currentIndex()-1] \
-                        + '/nodes/new_wired_status'
-        self.fw_status_text = self.browser.get(fw_status_url).text
-
-        # ddns value can be found by searching for '"dynamic_dns_name"'
-        ddns_value_start = self.fw_status_text.find("dynamic_dns_name")+19
-        ddns_value_end = self.fw_status_text[ddns_value_start:].find('\"') + ddns_value_start
-        self.current_ddns=self.fw_status_text[ddns_value_start: ddns_value_end]
-
-        # Primary will always come first, so using find should find it's IP address, even if there's a warm spare
-        # Using unique '{"public_ip":' to find primary IP address
-        ip_start = self.fw_status_text.find("{\"public_ip\":")+14
-        ip_end = self.fw_status_text[ip_start:].find('\"') + ip_start
-        self.current_primary_ip=self.fw_status_text[ip_start: ip_end]
 
     def main_init_ui(self):
         # Create a horizontal line above the status bar to highlight it
